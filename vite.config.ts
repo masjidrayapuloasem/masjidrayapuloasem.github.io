3ea89ac53a6658aa0ba523993e2453c8f2b995a0
Jeffry Luqman <jeffry.luqman@gmail.com>
2026-01-28 16:15:18 +0700

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { componentTagger } from "lovable-tagger";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/masjidrayapuloasem/" : "/",
   server: {
     host: "::",
     port: 8080,
